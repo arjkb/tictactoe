12542fa552a1d3e9b2802ce6e0afff24035144e3
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-11-01 12:05:42 -0600

    test isValidBoard() with multiple valid boards

--- tictactoe_test.go
+++ tictactoe_test.go
@@ -2,21 +2,25 @@ package tictactoe
 
 import "testing"
 
-func TestFoo(t *testing.T)  {
+func TestFoo(t *testing.T) {
 
-  s := foo(100);
-  if s != 10000 {
-    t.Error("Whoa!")
-  }
+	s := foo(100)
+	if s != 10000 {
+		t.Error("Whoa!")
+	}
 
 }
 
-func TestIsValid(t *testing.T)  {
-  validBoards := [...]string{"X--|XXX|OXO", "X--|XXX|OXO"}
+func TestIsValid(t *testing.T) {
+	validBoards := [...]string{"X--|XXX|OXO",
+		"X--|XXX|OXO",
+		"XXX|XXX|XXX",
+		"OOO|OOO|OOO",
+		"---|---|---"}
 
-  for _, validBoard := range validBoards {
-    if !isValidBoard(validBoard) {
-      t.Error(" Fails for valid board ", validBoard)
-    }
-  }
+	for _, validBoard := range validBoards {
+		if !isValidBoard(validBoard) {
+			t.Error(" Fails for valid board ", validBoard)
+		}
+	}
 }
