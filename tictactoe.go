9f6aa4a12cd3503f441108b1f68884db81a38cb9
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-11-01 15:30:54 -0600

    remove unnecessary lines of code

--- tictactoe.go
+++ tictactoe.go
@@ -21,20 +21,16 @@ func IsValidBoard(b string) bool {
 }
 
 func hasValidCharsOnly(board string) bool {
-
 	for i, ch := range board {
 		if i == 3 || i == 7 {
 			if ch != '|' {
-				// fmt.Printf("A %d %c %s", i, ch, board)
 				return false
 			}
 		} else {
 			if ch != 'X' && ch != 'O' && ch != '-' {
-				// fmt.Printf("B %d %c %s", i, ch, board)
 				return false
 			}
 		}
 	}
-
 	return true
 }
