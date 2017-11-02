84b8d4bab156ef2e3dbe79956d69c7abf2be77d4
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-11-01 18:24:11 -0600

    define func isIndicesDifferent() to check is all indices passed to it in [3]int are different

--- tictactoe.go
+++ tictactoe.go
@@ -1,8 +1,8 @@
 package tictactoe
 
 import (
-  "strings"
-  "fmt"
+	"fmt"
+	"strings"
 )
 
 func foo(num int) int {
@@ -23,24 +23,24 @@ func IsValidBoard(b string) bool {
 	return true
 }
 
-func IsWinnable(board string, ch byte, indices []int) (bool, error)  {
-  var count int
+func IsWinnable(board string, ch byte, indices []int) (bool, error) {
+	var count int
 
-  for _, index := range indices  {
-    if !isValidIndex(index) {
-      return false, fmt.Errorf("IsWinnable: index %d is outside the board")
-    }
+	for _, index := range indices {
+		if !isValidIndex(index) {
+			return false, fmt.Errorf("IsWinnable: index %d is outside the board")
+		}
 
-    if board[index] == ch {
-      count++
-    }
-  }
+		if board[index] == ch {
+			count++
+		}
+	}
 
-  if count == 2 {
-    return true, nil
-  } else {
-    return false, nil
-  }
+	if count == 2 {
+		return true, nil
+	} else {
+		return false, nil
+	}
 }
 
 func hasValidCharsOnly(board string) bool {
@@ -58,12 +58,20 @@ func hasValidCharsOnly(board string) bool {
 	return true
 }
 
-func isValidIndex(i int) bool  {
-  // 0123456789X
-  // ---|---|---
-  if i >= 0 && i <= 10  {
-    return true
-  } else {
-    return false
-  }
+func isValidIndex(i int) bool {
+	// 0123456789X
+	// ---|---|---
+	if i >= 0 && i <= 10 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func isIndicesDifferent(idx [3]int) bool {
+	if idx[0] == idx[1] || idx[0] == idx[2] || idx[1] == idx[2] {
+		return false
+	} else {
+		return true
+	}
 }
