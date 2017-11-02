157caedb1e3808c167b51a6acc3fdfdad25c504a
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-11-01 18:24:33 -0600

    test isIndicesDifferent(). ALL PASS.

--- tictactoe_test.go
+++ tictactoe_test.go
@@ -32,35 +32,78 @@ func TestIsValid(t *testing.T) {
 	}
 }
 
-func TestIsWinnable(t *testing.T)  {
-  winnable_board := "X--|---|X--"
+func TestIsWinnable(t *testing.T) {
+	winnable_board := "X--|---|X--"
 
-  winning_indices := []int{0,4,8}
+	winning_indices := []int{0, 4, 8}
 
-  win, _ := IsWinnable(winnable_board, 'X', winning_indices);
+	// indices := map[[3]int]bool  {
+	//   {1,2,3}:false,
+	//   {0,4,8}:true,
+	//   {5,5,5}
+	// }
 
-  if !win {
-    t.Error(" Doesn't show win for %v, %v", winnable_board, winning_indices)
-  }
+	win, _ := IsWinnable(winnable_board, 'X', winning_indices)
+
+	if !win {
+		t.Error(" Doesn't show win for %v, %v", winnable_board, winning_indices)
+	}
 }
 
-func TestIsValidIndex(t *testing.T)  {
-  indexes := map[int]bool  {
-    -10:false,
-    -1: false,
-    0:true,
-    1:true,
-    5:true,
-    9:true,
-    10:true,
-    11:false,
-    15:false,
-  }
+// func TestIsWinnable_All(t *testing.T)  {
+//   row_indices := [][]int  {
+//     {0,1,2},
+//     {4,5,6},
+//     {8,9,10},
+//   }
+//
+//   col_indices := [][]int {
+//     {0, 4, 8},
+//     {1, 5, 9},
+//     {2, 6, 10},
+//   }
+//
+//   diag_indices := [][]int {
+//     {0, 5, 10},
+//     {2, 5, 8},
+//   }
+//
+// }
 
-  for key, expected := range indexes  {
-    if isValidIndex(key) != expected  {
-      t.Errorf(" isValidIndex[%d] expected:%v, actual:%v", key, expected, isValidIndex(key))
-    }
-  }
+func TestIsValidIndex(t *testing.T) {
+	indexes := map[int]bool{
+		-10: false,
+		-1:  false,
+		0:   true,
+		1:   true,
+		5:   true,
+		9:   true,
+		10:  true,
+		11:  false,
+		15:  false,
+	}
 
+	for key, expected := range indexes {
+		if isValidIndex(key) != expected {
+			t.Errorf(" isValidIndex[%d] expected:%v, actual:%v", key, expected, isValidIndex(key))
+		}
+	}
+}
+
+func TestIsIndicesDifferent(t *testing.T) {
+	indices := map[[3]int]bool{
+		{1, 2, 3}: true,
+		{5, 5, 5}: false,
+		{2, 2, 3}: false,
+		{4, 5, 4}: false,
+		{6, 4, 4}: false,
+	}
+
+	for idxTriplet, expected := range indices {
+		actual := isIndicesDifferent(idxTriplet)
+
+		if actual != expected {
+			t.Errorf(" isIndicesDifferent[%v] expected:%v, actual:%v", idxTriplet, expected, actual)
+		}
+	}
 }
